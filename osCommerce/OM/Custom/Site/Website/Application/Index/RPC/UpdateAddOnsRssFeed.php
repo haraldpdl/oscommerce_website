befdacd0e719d51fef1ca6f3ee5295e9a624c028
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-11-03 03:08:35 +0100

    Add Add-Ons rss feed

---
+++ osCommerce/OM/Custom/Site/Website/Application/Index/RPC/UpdateAddOnsRssFeed.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * osCommerce Website
+ *
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+ * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ */
+
+  namespace osCommerce\OM\Core\Site\Website\Application\Index\RPC;
+
+  use osCommerce\OM\Core\HttpRequest;
+  use osCommerce\OM\Core\OSCOM;
+  use osCommerce\OM\Core\Site\RPC\Controller as RPC;
+
+  class UpdateAddOnsRssFeed {
+    public static function execute() {
+      $result = array();
+
+      if ( isset($_POST['key']) && ($_POST['key'] == OSCOM::getConfig('cron_key')) ) {
+        $feed_1 = HttpRequest::getResponse(array('url' => 'http://addons.oscommerce.com/rss/addons.rdf'));
+        $feed_2 = HttpRequest::getResponse(array('url' => 'http://addons.oscommerce.com/rss/packages.rdf'));
+
+        if ( !empty($feed_1) && !empty($feed_2) ) {
+          if ( (file_put_contents(OSCOM::getConfig('dir_fs_public', 'OSCOM') . 'sites/Website/rss/addons.xml', $feed_1, LOCK_EX) !== false) && (file_put_contents(OSCOM::getConfig('dir_fs_public', 'OSCOM') . 'sites/Website/rss/packages.xml', $feed_2, LOCK_EX) !== false) ) {
+            $result['rpcStatus'] = RPC::STATUS_SUCCESS;
+          }
+        }
+      }
+
+      if ( !isset($result['rpcStatus']) ) {
+        $result['rpcStatus'] = RPC::STATUS_NO_ACCESS;
+      }
+
+      echo json_encode($result);
+    }
+  }
+?>
