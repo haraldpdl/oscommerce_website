dfc6e68bf0e055ac267eff0e17ffdc8cdc1795c5
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-11-08 19:32:54 +0100

    Add social connection links to the footer

--- osCommerce/OM/Custom/Site/Website/languages/en_US.php
+++ osCommerce/OM/Custom/Site/Website/languages/en_US.php
@@ -12,3 +12,14 @@ navbutton_services = Services
 navbutton_support = Support
 
 copyright_footer = <p>Copyright (c) {value}current_year{value} osCommerce. All rights reserved. osCommerce is a registered trademark of Harald Ponce de Leon.</p>
+
+social_newsletter = Newsletter
+social_newsletter_title = Subscribe to our Newsletter
+social_googleplus = Google+
+social_googleplus_title = Circle us on Google+
+social_twitter = Twitter
+social_twitter_title = Follow us on Twitter
+social_facebook = Facebook
+social_facebook_title = Be our fan on Facebook
+social_github = GitHub
+social_github_title = Code with us on GitHub
