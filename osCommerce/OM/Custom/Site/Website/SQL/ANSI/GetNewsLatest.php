45fdf85021130417ee789116015ee725c63135e6
Harald Ponce de Leon <hpdl@oscommerce.com>
2014-03-04 03:11:54 +0100

    use prepare() due to caching

--- osCommerce/OM/Custom/Site/Website/SQL/ANSI/GetNewsLatest.php
+++ osCommerce/OM/Custom/Site/Website/SQL/ANSI/GetNewsLatest.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * osCommerce Website
- * 
- * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ *
+ * @copyright Copyright (c) 2014 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
@@ -14,7 +14,7 @@
     public static function execute() {
       $OSCOM_PDO = Registry::get('PDO');
 
-      $Qnews = $OSCOM_PDO->query('select id, title, date_added from :table_website_news where status = 1 order by date_added desc, title limit 1');
+      $Qnews = $OSCOM_PDO->prepare('select id, title, date_added from :table_website_news where status = 1 order by date_added desc, title limit 1');
       $Qnews->setCache('website-news-listing-latest_slim');
       $Qnews->execute();
 
