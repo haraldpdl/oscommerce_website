556da09d2a794d194b3e387f3bc82b502efd8953
Harald Ponce de Leon <hpdl@oscommerce.com>
2014-06-10 18:19:11 +0200

    control carousel through json file

--- osCommerce/OM/Custom/Site/Website/Module/Template/Widget/highlights/Controller.php
+++ osCommerce/OM/Custom/Site/Website/Module/Template/Widget/highlights/Controller.php
@@ -1,24 +1,52 @@
 <?php
 /**
  * osCommerce Website
- * 
- * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+ *
+ * @copyright Copyright (c) 2014 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
   namespace osCommerce\OM\Core\Site\Website\Module\Template\Widget\highlights;
 
+  use osCommerce\OM\Core\HTML;
   use osCommerce\OM\Core\OSCOM;
+  use osCommerce\OM\Core\Registry;
 
   class Controller extends \osCommerce\OM\Core\Template\WidgetAbstract {
     static public function execute($param = null) {
+      $OSCOM_Template = Registry::get('Template');
+
       $file = OSCOM::BASE_DIRECTORY . 'Custom/Site/' . OSCOM::getSite() . '/Module/Template/Widget/highlights/pages/main.html';
 
       if ( !file_exists($file) ) {
         $file = OSCOM::BASE_DIRECTORY . 'Core/Site/' . OSCOM::getSite() . '/Module/Template/Widget/highlights/pages/main.html';
       }
 
-      return file_get_contents($file);
+      $result = '';
+
+      if ( file_exists(__DIR__ . '/data.json') ) {
+        $data = json_decode(file_get_contents(__DIR__ . '/data.json'), true);
+
+        if ( !empty($data) && is_array($data) ) {
+          $counter = 1;
+
+          foreach ( $data as $p ) {
+            $result .= '<div class="' . (($counter === 1) ? 'active ' : '') . 'item">
+  <a href="' . $p['url'] . '"' . (($p['newwin'] === true) ? ' target="_blank"' : '') . '>' . (($p['partner'] === true) ? '<span class="label label-warning" style="position: absolute; padding: 7px;">' . OSCOM::getDef('tag_partner') . '</span>' : '') . '<img src="' . $p['image'] . '" ' . (!empty($p['title']) ? 'title="' . HTML::outputProtected($p['title']) . '" ' : '') . 'width="940" height="285"/></a>
+</div>';
+
+            $counter++;
+          }
+        }
+      }
+
+      if ( !empty($result) ) {
+        $OSCOM_Template->setValue('highlights_carousel_output', $OSCOM_Template->parseContent($result));
+
+        return file_get_contents($file);
+      }
+
+      return '';
     }
   }
 ?>
--- osCommerce/OM/Custom/Site/Website/languages/en_US/Modules/Template/Widgets/highlights.php
+++ osCommerce/OM/Custom/Site/Website/languages/en_US/Modules/Template/Widgets/highlights.php
@@ -1,8 +1,6 @@
 # osCommerce Website
 #
-# @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+# @copyright Copyright (c) 2014 osCommerce; http://www.oscommerce.com
 # @license BSD License; http://www.oscommerce.com/bsdlicense.txt
 
-highlight_1_caption = Our latest product: osCommerce Online Merchant v3.0
-highlight_3_caption = PayPal ready! Start selling and receive money instantly!
-highlight_4_caption = Placeholder
+tag_partner = Partner
