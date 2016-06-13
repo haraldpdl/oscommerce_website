e0dee7ded3a23c205da5c680fd68c5dc62a92d2b
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-06-14 00:37:15 +0200

    standardize layout and return link

--- osCommerce/OM/Custom/Site/Website/Module/Template/Widget/account_sidebar_nav/Controller.php
+++ osCommerce/OM/Custom/Site/Website/Module/Template/Widget/account_sidebar_nav/Controller.php
@@ -2,23 +2,39 @@
 /**
  * osCommerce Website
  *
- * @copyright Copyright (c) 2014 osCommerce; http://www.oscommerce.com
- * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ * @copyright (c) 2016 osCommerce; https://www.oscommerce.com
+ * @license BSD; https://www.oscommerce.com/bsdlicense.txt
  */
 
-  namespace osCommerce\OM\Core\Site\Website\Module\Template\Widget\account_sidebar_nav;
+namespace osCommerce\OM\Core\Site\Website\Module\Template\Widget\account_sidebar_nav;
 
-  use osCommerce\OM\Core\OSCOM;
+use osCommerce\OM\Core\{
+  OSCOM,
+  Registry
+};
 
-  class Controller extends \osCommerce\OM\Core\Template\WidgetAbstract {
-    static public function execute($param = null) {
-      $file = OSCOM::BASE_DIRECTORY . 'Custom/Site/' . OSCOM::getSite() . '/Module/Template/Widget/account_sidebar_nav/pages/main.html';
+class Controller extends \osCommerce\OM\Core\Template\WidgetAbstract
+{
+    public static function execute($param = null)
+    {
+        $OSCOM_Application = Registry::get('Application');
+        $OSCOM_Template = Registry::get('Template');
 
-      if ( !file_exists($file) ) {
-        $file = OSCOM::BASE_DIRECTORY . 'Core/Site/' . OSCOM::getSite() . '/Module/Template/Widget/account_sidebar_nav/pages/main.html';
-      }
+        $req = $OSCOM_Application->getRequestedActions();
 
-      return file_get_contents($file);
+        if ((count($req) > 1) && ($req[0] == 'Partner')) {
+            $OSCOM_Template->setValue('sidebar_nav_extra_link', [
+                'url' => OSCOM::getLink(null, 'Account', 'Partner', 'SSL'),
+                'title' => OSCOM::getDef('partner_back_to_campaigns')
+            ]);
+        }
+
+        $file = OSCOM::BASE_DIRECTORY . 'Custom/Site/' . OSCOM::getSite() . '/Module/Template/Widget/account_sidebar_nav/pages/main.html';
+
+        if (!file_exists($file)) {
+            $file = OSCOM::BASE_DIRECTORY . 'Core/Site/' . OSCOM::getSite() . '/Module/Template/Widget/account_sidebar_nav/pages/main.html';
+        }
+
+        return file_get_contents($file);
     }
-  }
-?>
+}
