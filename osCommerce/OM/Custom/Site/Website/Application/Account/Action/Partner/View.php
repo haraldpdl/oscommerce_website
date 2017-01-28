d46abdbf1f0e24c17e3e4359e4fe7d8953bf2c12
Harald Ponce de Leon <hpdl@oscommerce.com>
2017-01-28 23:51:53 +0100

    allow to preview in all languages

--- osCommerce/OM/Custom/Site/Website/Application/Account/Action/Partner/View.php
+++ osCommerce/OM/Custom/Site/Website/Application/Account/Action/Partner/View.php
@@ -19,6 +19,7 @@ class View
 {
     public static function execute(ApplicationAbstract $application)
     {
+        $OSCOM_Language = Registry::get('Language');
         $OSCOM_Template = Registry::get('Template');
 
         if (empty($_GET['View']) || !Partner::hasCampaign($_SESSION[OSCOM::getSite()]['Account']['id'], $_GET['View'])) {
@@ -30,6 +31,66 @@ class View
         $partner = Partner::get($_GET['View']);
 
         $OSCOM_Template->setValue('partner', $partner);
+
+        $partner_campaign = Partner::getCampaign($_SESSION[OSCOM::getSite()]['Account']['id'], $_GET['View']);
+
+        $campaign = [
+            'app_code' => $partner_campaign['app_code'],
+            'info' => []
+        ];
+
+        foreach ($OSCOM_Language->getAll() as $l) {
+            $info = Partner::getCampaignInfo($partner['id'], $l['id']);
+
+            $campaign['info'][$l['code']] = [
+                'title' => !empty($info['title']) ? $info['title'] : null,
+                'desc_long' => !empty($info['desc_long']) ? nl2br($OSCOM_Template->parseContent($info['desc_long'], ['b', 'u', 'i', 'url', 'ul'])) : null,
+                'url' => !empty($info['url']) ? $info['url'] : null,
+                'public_url' => !empty($info['public_url']) ? $info['public_url'] : null,
+                'email' => !empty($info['email']) ? $info['email'] : null,
+                'telephone' => !empty($info['telephone']) ? $info['telephone'] : null,
+                'address' => !empty($info['address']) ? nl2br($info['address']) : null,
+                'image_small_filename' => !empty($info['image_small']) ? $info['image_small'] : null,
+                'image_small' => null,
+                'image_big_filename' => !empty($info['image_big']) ? $info['image_big'] : null,
+                'image_big' => null,
+                'youtube_video_id' => !empty($info['youtube_video_id']) ? $info['youtube_video_id'] : null
+            ];
+        }
+
+        if (!empty($campaign['info']['en_US']['image_small_filename']) && file_exists(OSCOM::getConfig('dir_fs_public', 'OSCOM') . 'sites/' . OSCOM::getSite() . '/images/partners/en_US/' . $campaign['info']['en_US']['image_small_filename'])) {
+            $campaign['info']['en_US']['image_small'] = OSCOM::getPublicSiteLink('images/partners/en_US/' . $campaign['info']['en_US']['image_small_filename']);
+        }
+
+        unset($campaign['info']['en_US']['image_small_filename']);
+
+        if (!empty($campaign['info']['en_US']['image_big_filename']) && file_exists(OSCOM::getConfig('dir_fs_public', 'OSCOM') . 'sites/' . OSCOM::getSite() . '/images/partners/en_US/' . $campaign['info']['en_US']['image_big_filename'])) {
+            $campaign['info']['en_US']['image_big'] = OSCOM::getPublicSiteLink('images/partners/en_US/' . $campaign['info']['en_US']['image_big_filename']);
+        } else {
+            $campaign['info']['en_US']['image_big'] = OSCOM::getPublicSiteLink($OSCOM_Template->getValue('highlights_image'));
+        }
+
+        unset($campaign['info']['en_US']['image_big_filename']);
+
+        foreach ($campaign['info'] as $k => $v) {
+            if ($k != 'en_US') {
+                if (!empty($v['image_small_filename']) && file_exists(OSCOM::getConfig('dir_fs_public', 'OSCOM') . 'sites/' . OSCOM::getSite() . '/images/partners/' . $k . '/' . $v['image_small_filename'])) {
+                    $campaign['info'][$k]['image_small'] = OSCOM::getPublicSiteLink('images/partners/' . $k . '/' . $v['image_small_filename']);
+                }
+
+                unset($campaign['info'][$k]['image_small_filename']);
+
+                if (!empty($v['image_big_filename']) && file_exists(OSCOM::getConfig('dir_fs_public', 'OSCOM') . 'sites/' . OSCOM::getSite() . '/images/partners/' . $k . '/' . $v['image_big_filename'])) {
+                    $campaign['info'][$k]['image_big'] = OSCOM::getPublicSiteLink('images/partners/' . $k . '/' . $v['image_big_filename']);
+                }
+
+                unset($campaign['info'][$k]['image_big_filename']);
+            }
+        }
+
+        $OSCOM_Template->setValue('campaign_info', $campaign);
+
+        $OSCOM_Template->setValue('language_code', $OSCOM_Language->getCode());
 
         $application->setPageContent('partner_preview.html');
 
