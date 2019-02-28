c2d1fba5d37c4852c79c5134e150f4723ec7db92
Harald Ponce de Leon <hpdl@oscommerce.com>
2019-02-28 18:36:23 +0100

    fix merge

--- osCommerce/OM/Custom/Site/Website/SQL/Releases/GetVersions.php
+++ osCommerce/OM/Custom/Site/Website/SQL/Releases/GetVersions.php
@@ -16,7 +16,8 @@ class GetVersions
     {
         $OSCOM_PDO = Registry::get('PDO');
 
-        $Qversions = $OSCOM_PDO->prepare('select v.*, d.date, d.news_id from :table_website_release_versions v, :table_website_downloads d where v.downloads_id = d.id order by v.major_ver, v.minor_ver, v.patch_ver');
+//        $Qversions = $OSCOM_PDO->prepare('select v.*, d.date, d.news_id from :table_website_release_versions v, :table_website_downloads d where v.downloads_id = d.id order by v.major_ver, v.minor_ver, v.patch_ver');
+        $Qversions = $OSCOM_PDO->prepare('select * from :table_website_release_versions order by major_ver, minor_ver, patch_ver');
         $Qversions->setCache('website-releases-versions');
         $Qversions->execute();
 
@@ -30,9 +31,9 @@ class GetVersions
                 'minor_ver' => $Qversions->valueInt('minor_ver'),
                 'patch_ver' => $Qversions->valueInt('patch_ver'),
                 'has_apps' => ($Qversions->valueInt('has_apps') === 1),
-                'parent_id' => $Qversions->hasValue('parent_id') ? $Qversions->valueInt('parent_id') : null,
-                'date' => $Qversions->value('date'),
-                'news_id' => $Qversions->valueInt('news_id')
+//                'parent_id' => $Qversions->hasValue('parent_id') ? $Qversions->valueInt('parent_id') : null,
+//                'date' => $Qversions->value('date'),
+//                'news_id' => $Qversions->valueInt('news_id')
             ];
         }
 
