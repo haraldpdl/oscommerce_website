5c00a0d8e2d617e46623638974f198c3adcbd536
Harald Ponce de Leon <hpdl@oscommerce.com>
2019-05-28 03:47:29 +0200

    update session class

--- osCommerce/OM/Custom/Site/Website/Session.php
+++ osCommerce/OM/Custom/Site/Website/Session.php
@@ -8,10 +8,16 @@
 
 namespace osCommerce\OM\Core\Site\Website;
 
+use osCommerce\OM\Core\OSCOM;
+
 class Session extends \osCommerce\OM\Core\Session
 {
     public static function load($name = null)
     {
+        if (OSCOM::configExists('store_sessions', 'Website')) {
+            static::$driver = OSCOM::getConfig('store_sessions', 'Website');
+        }
+
         ini_set('session.use_cookies', '1');
         ini_set('session.use_only_cookies', '1');
         ini_set('session.cookie_secure', '1');
